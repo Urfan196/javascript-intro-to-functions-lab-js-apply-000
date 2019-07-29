659071f4f5cd37d99de7d217bfc417dde60a3781
Urfan Guliyev <urfan196@yahoo.com>
2019-07-29 01:05:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+shout("hello")
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+whsiper("HELLO")
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+logShout("hello")
+
+function logWhisper(string) {
+  console.console.log(string.toLowerCase());
+}
+logWhisper ("HELLO")
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (string === string.toLowerCase()){
+return "I can't hear you!"
+  } return "YES INDEED!"
+} 
